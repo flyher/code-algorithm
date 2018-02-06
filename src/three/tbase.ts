cd1a019061666737beb96c17567cda027690b838
Flyher D <flyher@users.noreply.github.com>
2018-02-06 18:02:40 +0800

    demo test threejs

--- src/three/tbase.ts
+++ src/three/tbase.ts
@@ -1,3 +1,6 @@
-import { Scene } from 'three';
+import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
 const scene = new Scene();
-
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
